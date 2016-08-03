306cf83e2537533cdd04cfeb05ef09ae674bdfbe
Nathan Rothenberger <nroth10@gmail.com>
2016-08-03 00:09:25 -0700

    Add empty component for route testing

---
+++ app/components/Empty.js
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var Empty = React.createClass({
+	render: function() {
+		return (
+			<div className="view">
+				<h1>empty</h1>
+			</div>
+		)
+	}
+})
+
+export default Empty;
