c2b601c581099905774ba131eb3a3d89e07ce0dd
Nathan Rothenberger <nroth10@gmail.com>
2016-08-04 00:20:25 -0700

    Add additional gulp and webpack packages. Add surge task, update styles task, edit npm scripts for new webpack config

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var notify = require('gulp-notify');
+var surge = require('gulp-surge');
+var shell = require('gulp-shell');
 
 var styles = {
 	src: 'styles/main.scss',
@@ -8,15 +10,38 @@ var styles = {
 	watch: 'styles/**/*.scss'
 }
 
+var src = {
+	styles: 'app/styles/main.scss',
+	watch: 'app/styles/**/*.scss'
+}
+
+var dest = {
+	styles: 'dist/styles',
+	public: 'dist',
+	surge: 'nathan-needs-a-yahb.surge.sh'
+}
+
 gulp.task('styles', function() {
-	return gulp.src(styles.src)
+	return gulp.src(src.styles)
 		.pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest(styles.dest));
+		.pipe(gulp.dest(dest.styles));
 });
 
 gulp.task('watch', function() {
 	// watch sass files
-	gulp.watch(styles.watch, ['styles']);
+	gulp.watch(src.watch, ['styles']);
 });
 
-gulp.task('default', function() { console.log('gulp just ran'); });
+
+gulp.task('default', ['dev'], function() { 
+	console.log('gulp just ran'); 
+});
+
+
+
+// WEBPACK STUFF
+gulp.task('dev', shell.task('npm run start'))
+gulp.task('production', shell.task('npm list -depth=0'));
+
+// DEPLOY
+gulp.task('deploy', function() {});
